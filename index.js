cd45f87dd5fa8c6daa7f46322b849f8aec0469e7
Vincent <vshan0816@gmail.com>
2021-09-29 14:44:31 -0400

    commit tree b16a640f85679f2d2e7b31c134b0e26444dfdf07

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+// function superbowlWin(array) {
+//     const win = array.find(element => element.result === "W")
+//     if (!win) {return undefined}
+//         else {
+//         return win.year
+//         }
+//     }
+
+
+function superbowlWin(array) {
+    const win = array.find(element => element.result === "W")
+    if (win){return win.year}
+}
